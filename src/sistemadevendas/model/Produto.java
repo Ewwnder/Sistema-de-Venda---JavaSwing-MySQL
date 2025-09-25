ec12022fbfdd24206f7af563dd227ebaff9b170d
LucasMorais42 <lucasmorais2214@gmail.com>
2025-09-25 20:03:57 -0300

    Envio correcoes de merge conflict

--- src/sistemadevendas/model/Produto.java
+++ src/sistemadevendas/model/Produto.java
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-package sistemadevendas.model;
-=======
-
 package sistemadevendas.model;
 
->>>>>>> 06a105ef78c799da3adf5f026a472e721347c2b0
 /**
  *
  * @author LucasMorais
@@ -14,10 +9,9 @@ public class Produto {
     private String nomeProduto;
     private String descricao;
     private double precoVenda;
-<<<<<<< HEAD
+
     private int quantidade;
-=======
->>>>>>> 06a105ef78c799da3adf5f026a472e721347c2b0
+
 
     public int getIdProduto() {
         return idProduto;
@@ -51,7 +45,6 @@ public class Produto {
         this.precoVenda = precoVenda;
     }
 
-<<<<<<< HEAD
     public int getQuantidade() {
         return quantidade;
     }
@@ -63,8 +56,7 @@ public class Produto {
     public String toString() {
         return nomeProduto;
     }
-=======
->>>>>>> 06a105ef78c799da3adf5f026a472e721347c2b0
+
     
     
     
